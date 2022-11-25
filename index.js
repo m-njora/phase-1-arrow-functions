62ea8f27fe71f3a6a7676245820591e822b084c7
m-njora <mmuthekijora@gmail.com>
2022-11-25 22:12:32 +0300

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide =(param1 = 2000, param2 = 100) =>{
+  return param1 / param2
+} 
+const square = (parameter) => {
+   return parameter*parameter
+}
+const add = (value1,value2) => {
+    return value1 + value2
+}
